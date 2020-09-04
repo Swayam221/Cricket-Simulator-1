fc7b413d70b6168a2a68a623fc94d1e5574f90a1
vasu23gupta <70284836+vasu23gupta@users.noreply.github.com>
2020-09-04 23:55:02 +0530

    player class

---
+++ Cricket Simulator/Player.h
@@ -0,0 +1,51 @@
+# include <string.h>
+# include <stdio.h>
+# include <math.h>
+class Player
+{
+	int totalRuns;
+	float strikeRate;
+	int fours;
+	int sixes;
+	int balls;
+	int ballsPlayed;
+	int wickets;
+	int ones;
+public:
+	char name[50];
+	char role[10];
+	float pFours;
+	float pSixes;
+	float pOnes;
+	float pWicket;
+	float pZero;
+	Player(char nm[], char rol[], int rns, float sr, int frs, int sxs, int bls, int wkts) {
+		strcpy_s(name, 50, nm);
+		strcpy_s(role, 10, rol);
+		totalRuns = rns;
+		strikeRate = sr;
+		fours = frs;
+		sixes = sxs;
+		balls = bls;
+		wickets = wkts;
+		ballsPlayed = totalRuns / (strikeRate / 100);
+		pFours = (fours * 1.0) / ballsPlayed;
+		pSixes = (sixes * 1.0) / ballsPlayed;
+		ones = (totalRuns - (fours * 4) - (sixes * 6));
+		pOnes = (ones * 1.0) / ballsPlayed;
+		pZero = ((ballsPlayed - fours - sixes- ones)*1.0)/ballsPlayed;
+		if (balls != 0)
+		{
+			pWicket = wickets * 1.0 / balls;
+		}
+	}
+
+	bool canBall() {
+		if (strcmp(role, "Ball") == 0 || strcmp(role, "Both")) {
+			return true;
+		}
+		return false;
+	}
+
+	~Player();
+};
